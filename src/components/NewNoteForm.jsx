c5211cc54d2fb2d1e2c3c45243b08bdf006bd0cf
Alfonsus Ardani <alfonsusac@gmail.com>
2024-01-30 19:23:45 +0700

    fix svg + reset state on modal open

--- src/components/NewNoteForm.jsx
+++ src/components/NewNoteForm.jsx
@@ -9,13 +9,13 @@ export const NewNoteForm = () => {
   const [title, setTitle] = useState("")
   const [desc, setDesc] = useState("")
   const router = useRouter()
-  const [_, setIsAdding] = useAtom(isNewPost)
+  const [isAdding, setIsAdding] = useAtom(isNewPost)
   const [formState, setFormState] = useAtom(newPostFormState)
 
   useEffect(() => {
-    setFormState("idle")
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [setFormState])
+    if(isAdding === true)
+      setFormState("idle")
+  }, [setFormState, isAdding])
 
   function isValid() {
     if (title === "") return false
@@ -99,13 +99,13 @@ export const NewNoteForm = () => {
 
 export function LineMdConfirmCircle(props) {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...props}><g fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"><path strokeDasharray="60" strokeDashoffset="60" d="M3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12Z"><animate fill="freeze" attributeName="stroke-dashoffset" dur="0.5s" values="60;0"></animate></path><path strokeDasharray="14" stroke-dashoffset="14" d="M8 12L11 15L16 10"><animate fill="freeze" attributeName="stroke-dashoffset" begin="0.6s" dur="0.2s" values="14;0"></animate></path></g></svg>
+    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...props}><g fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"><path strokeDasharray="60" strokeDashoffset="60" d="M3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12Z"><animate fill="freeze" attributeName="strokeDashoffset" dur="0.5s" values="60;0"></animate></path><path strokeDasharray="14" strokeDashoffset="14" d="M8 12L11 15L16 10"><animate fill="freeze" attributeName="strokeDashoffset" begin="0.6s" dur="0.2s" values="14;0"></animate></path></g></svg>
   )
 }
 
 
 export function LineMdLoadingTwotoneLoop(props) {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...props}><g fill="none" stroke="currentColor" strokeLinecap="round" strokeWidth="2"><path strokeDasharray="60" strokeDashoffset="60" strokeOpacity=".3" d="M12 3C16.9706 3 21 7.02944 21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3Z"><animate fill="freeze" attributeName="stroke-dashoffset" dur="1.3s" values="60;0"></animate></path><path strokeDasharray="15" strokeDashoffset="15" d="M12 3C16.9706 3 21 7.02944 21 12"><animate fill="freeze" attributeName="stroke-dashoffset" dur="0.3s" values="15;0"></animate><animateTransform attributeName="transform" dur="1.5s" repeatCount="indefinite" type="rotate" values="0 12 12;360 12 12"></animateTransform></path></g></svg>
+    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...props}><g fill="none" stroke="currentColor" strokeLinecap="round" strokeWidth="2"><path strokeDasharray="60" strokeDashoffset="60" strokeOpacity=".3" d="M12 3C16.9706 3 21 7.02944 21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3Z"><animate fill="freeze" attributeName="strokeDashoffset" dur="1.3s" values="60;0"></animate></path><path strokeDasharray="15" strokeDashoffset="15" d="M12 3C16.9706 3 21 7.02944 21 12"><animate fill="freeze" attributeName="strokeDashoffset" dur="0.3s" values="15;0"></animate><animateTransform attributeName="transform" dur="1.5s" repeatCount="indefinite" type="rotate" values="0 12 12;360 12 12"></animateTransform></path></g></svg>
   )
 }
